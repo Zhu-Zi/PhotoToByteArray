2a6f5e93ff6e553e37ba13ae2540fc12b9c380de
Zhu-Zi <513444173@qq.com>
2015-08-23 19:46:37 +0800

    PhotoToArray
    Making photos transorm Byte[] by C#

---
+++ PhotoTransformByte[]/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace PhotoTransformByte__
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 465);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "转码";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 511);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "图片";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(32, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(638, 422);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 466);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(408, 466);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "查询";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 512);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(187, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 546);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ PhotoTransformByte[]/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Data.SQLite;
+
+namespace PhotoTransformByte__
+{
+    public partial class Form1 : Form
+    {
+        string ConnStr = @"Data Source=E:\环境&Navicat\库文件\001.db;";//建立连接字符串
+        SQLiteConnection sqlconn;//连接对象
+        SQLiteCommand cmd;//SQLite命令对象
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "选择要转换的图片";
+            dlg.Filter = "Image files (*.jpg;*.bmp;*.gif)|*.jpg*.jpeg;*.gif;*.bmp|AllFiles (*.*)|*.*";
+            if (DialogResult.OK == dlg.ShowDialog())
+            {
+                ImgToBase64String(dlg.FileName);
+            }  
+        }
+        private void ImgToBase64String(string Imagefilename)
+        {
+            try
+            {
+                Bitmap bmp = new Bitmap(Imagefilename);
+                this.pictureBox1.Image = bmp;
+                FileStream fs = new FileStream(Imagefilename + ".txt", FileMode.Create);
+                StreamWriter sw = new StreamWriter(fs);
+
+                MemoryStream ms = new MemoryStream();
+                bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                byte[] arr = new byte[ms.Length];
+                ms.Position = 0;
+                ms.Read(arr, 0, (int)ms.Length);
+                ms.Close();
+                String strbaser64 = Convert.ToBase64String(arr);
+                sw.Write(strbaser64);
+
+
+                sqlconn = new SQLiteConnection(ConnStr);//建立SQLite连接
+                sqlconn.Open();
+
+                cmd = new SQLiteCommand(sqlconn);//建立SQLite命令对象
+                string sqlline = "insert into SQLite04 values("+ textBox1.Text +",'" + strbaser64 + "')";//构造SQL语句
+                cmd.CommandText = sqlline;//向命令对象传递SQL语句
+                cmd.ExecuteNonQuery();//执行SQL语句,返回更改行数
+                sqlconn.Close();
+
+
+                sw.Close();
+                fs.Close();
+                MessageBox.Show("转换成功!");             
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ImgToBase64String 转换失败/nException:" + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "选择要转换的base64编码的文本";
+            dlg.Filter = "txt files|*.txt";
+            if (DialogResult.OK == dlg.ShowDialog())
+            {
+                Base64StringToImage(dlg.FileName);
+            }  
+        }
+        private void Base64StringToImage(string txtFileName)
+        {
+            try
+            {
+                FileStream ifs = new FileStream(txtFileName, FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(ifs);
+
+                String inputStr = sr.ReadToEnd();
+                byte[] arr = Convert.FromBase64String(inputStr);
+                MemoryStream ms = new MemoryStream(arr);
+                Bitmap bmp = new Bitmap(ms);
+
+                bmp.Save(txtFileName + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                //bmp.Save(txtFileName + ".bmp", ImageFormat.Bmp);  
+                //bmp.Save(txtFileName + ".gif", ImageFormat.Gif);  
+                //bmp.Save(txtFileName + ".png", ImageFormat.Png);  
+                ms.Close();
+                sr.Close();
+                ifs.Close();
+                this.pictureBox1.Image = bmp ;
+                MessageBox.Show("转换成功！");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Base64StringToImage 转换失败/nException：" + ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sqlconn = new SQLiteConnection(ConnStr);//建立SQLite连接
+            sqlconn.Open();
+
+            cmd = new SQLiteCommand(sqlconn);//建立SQLite命令对象
+            string sqlline = "select 照片 from SQLite04 where 学号=" + textBox1.Text;//构造SQL语句
+            cmd.CommandText = sqlline;//向命令对象传递SQL语句
+
+            SQLiteDataReader sldr ;
+            sldr = cmd.ExecuteReader();//将sqlline中的值写入sldr
+           
+
+//          textBox2.Text = sldr.GetString(1);
+
+            StringBuilder photo = new StringBuilder();
+            while (sldr.Read())
+            {
+                photo.Append(sldr.GetString(0));
+            }
+   //         textBox2.Text = photo.ToString();
+
+            sqlconn.Close();
+
+            String inputStr = photo.ToString();
+            byte[] arr = Convert.FromBase64String(inputStr);
+            MemoryStream ms = new MemoryStream(arr);
+            Bitmap bmp = new Bitmap(ms);
+            ms.Close();
+
+            this.pictureBox1.Image = bmp;
+        }
+
+    }
+}
